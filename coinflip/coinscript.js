639d60d454356208884cb1335b8843d30354550b
Ishani Koul <kouli518@newschool.edu>
2025-04-09 01:31:16 -0400

    coin flip fix

--- coinflip/coinscript.js
+++ coinflip/coinscript.js
@@ -4,7 +4,7 @@ let flipped = false;
 
 flipButton.addEventListener('click', () => {
 	const isTrue = Math.random() < 0.5;
-	const rotation = isTrue ? 1800 : 1980;
+	const rotation = isTrue ? 1800 : 1980; //Spin 5 and 5.5 times
 
   coin.style.transform = `rotateY(${rotation}deg)`;
 });
